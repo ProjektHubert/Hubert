d98847bdb7af8082a8422d33628dfd0b3f66c7a3
Jens Stanny Olafsson <jens703i@edu.easj.dk>
2018-05-09 11:23:43 +0200

    1,001

---
+++ Projekt/Projekt/Model/produkt.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt.Model
+{
+    class Produkt
+    {
+        private int _produktID;
+        private string _produktNavn;
+        private int _martrialeID;
+        private int _størrelse;
+        private int _typeID;
+        private string _pris;
+    }
+
+    public Produkt() 
+        {
+        }
+    public int ProduktID
+    {
+        get => _produktID;
+        set => _produktID = value; 
+    }
+     public string ProduktNavn
+    {
+        get => _produktNavn;
+        set => _produktNavn = value; 
+    }
+    public int MartrialeID
+    {
+        get => _martrialeID;
+        set => _martrialeID = value;
+    }
+    public int Størrelse
+    {
+        get => _størrelse;
+        set => _størrelse = value;
+    }
+    private int TypeID
+    {
+        get => _typeID;
+        set => _typeID = value;
+    }
+    private string Pris
+    {
+        get => _pris;
+        set => _pris = value;
+    }
+
+}
